f4f1c3b05d460abb777976e29d778c618aa31d5d
lina2360 <49011412+lina2360@users.noreply.github.com>
2020-10-25 15:14:44 +0800

    Add files via upload

---
+++ week_5/HW/Main.java
@@ -0,0 +1,20 @@
+package ta;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()){
+			String url = sc.next();
+			HtmlMatcher hm = new HtmlMatcher(url);
+			hm.match();
+		}
+		
+
+	}
+
+}
