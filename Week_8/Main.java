c52a476befdc54f6e4cd3bbe0d2b70a3f58246b8
Chih-Kai Jan <skyjan0428@gmail.com>
2020-11-16 12:10:28 +0800

    Update Main.java

--- Week_8/Main.java
+++ Week_8/Main.java
@@ -2,10 +2,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		HanoiTower t = new HanoiTower(5);
+		testExecute(5);
+		
+		//testPrintStack(5);
+
+	}
+	public static void testPrintStack(int desk) {
+		MyStack<Integer> rod = new MyStack();
+		for(int i=desk; i>=1; i--) {
+			rod.push(i);
+		}
+		rod.printStack();
+	}
+	public static void testExecute(int desk) {
+		HanoiTower t = new HanoiTower(desk);
 		t.start();
 		t.printHanoiTower();
-
+		
 	}
 
 }
